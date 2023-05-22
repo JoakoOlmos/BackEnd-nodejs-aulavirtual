06ae11c1f251375978c0e4cd6eb8a47759b3b277
JoakoOlmos <joakolmos.14@gmail.com>
2023-05-22 18:58:03 -0300

    add put controller

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const app = express();
 // Configurar body-parser para analizar las solicitudes en formato JSON
 app.use(bodyParser.json());
 app.use(cors());
-
+let perfil = {
+  nombre: 'Joaquin',
+  edad: 26,
+  apellido: 'Olmos',
+  trabajando: true,
+  telefonos: {
+      movil: 35165156,
+      fijo: 5646546,
+  }
+}
 // Definir las rutas y controladores de tu API REST
 // Ejemplo:
 app.get('/saludar', (req, res) => {
@@ -19,19 +28,13 @@ app.get('/despedir', (req, res) => {
   });
 
   app.get('/perfil', (req, res) => {
-    const perfil = {
-        nombre: 'Joaquin',
-        edad: 26,
-        apellido: 'Olmos',
-        trabajando: true,
-        telefonos: {
-            movil: 35165156,
-            fijo: 5646546,
-        }
-    }
     res.send(perfil);
   });
 
+  app.put('/perfil', (req, res) => {
+    perfil = req.body;
+    res.send ('El usuario se ha modificado correctamente.')
+  });
 // Arrancar el servidor
 const port = 3000;
 app.listen(port, () => {
